044533792447582fd4a4120222a29a85ef2e33bf
maphnew <zcm3118@gmail.com>
2024-03-21 18:31:44 +0900

    가계부 만들기 돌입

---
+++ account-book/index.js
@@ -0,0 +1,24 @@
+const datepicker = document.querySelector('#datepicker')
+const dateVal = datepicker.value
+
+const now = new Date()
+
+if(!dateVal) {
+    const today = now.getFullYear() + '-' + leftFullNum(now.getMonth()+1, 2) + '-' + leftFullNum(now.getDate(), 2)
+    datepicker.value = today
+}
+
+function leftFullNum(num, targetLength) {
+    return num.toString().padStart(targetLength, "0")
+}
+
+datepicker.addEventListener('change', function(e) {
+    const pickedDate = new Date(e.target.value)
+    const day = pickedDate.getDay()
+    whatday.value = dayNames[day]
+})
+
+const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const day = now.getDay()
+const whatday = document.querySelector('#whatday')
+whatday.value = dayNames[day]
