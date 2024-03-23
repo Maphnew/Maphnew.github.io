8ec5a8d3771f9819d5a24d9d53fecee23320d4a5
maphnew <zcm3118@gmail.com>
2024-03-23 22:46:42 +0900

    중복문제 제거

--- account-book/components/Contents.js
+++ account-book/components/Contents.js
@@ -15,11 +15,11 @@ export default function contents({ $target, initialState }) {
             <input type="text" value="${item.name}">
             <input type="number" value="${item.price}">
             <div>
-                <input type="radio" id="cash" name="payment-${index}" value="cash" ${item.payment === "cash" ? "checked" : ""}>
-                    <label for="cash">현금</label>
+                <input type="radio" id="cash-${index}" name="payment-${index}" value="cash" ${item.payment === "cash" ? "checked" : ""}>
+                    <label for="cash-${index}">현금</label>
                 </input>
-                <input type="radio" id="card" name="payment-${index}" value="card" ${item.payment === "card" ? "checked" : ""}>
-                    <label for="card">카드</label>
+                <input type="radio" id="card-${index}" name="payment-${index}" value="card" ${item.payment === "card" ? "checked" : ""}>
+                    <label for="card-${index}">카드</label>
                 </input>
             </div>
             <select>
